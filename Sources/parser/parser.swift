94f29e198ad180fb80c955c8a69b1b84b768a73b
xiote <xiote@nate.com>
2020-04-13 14:48:14 +0900

    parse, parseXml 함수 추가

--- Sources/parser/parser.swift
+++ Sources/parser/parser.swift
@@ -1,3 +1,26 @@
-struct Parser {
+import Foundation
+import XMLCoder
+
+struct OwnerInfo: Decodable, Equatable {
+    var sigunguCd: Int
+
+    enum CodingKeys: String, CodingKey {
+        case sigunguCd = "sigungu_cd"
+    }
+}
+
+class Parser {
     var text = "Hello, World!"
+
+    func parse(_ jsonString: String) -> OwnerInfo {
+        let data = jsonString.data(using: .utf8)!
+        let ownerInfo: OwnerInfo = try! JSONDecoder().decode(OwnerInfo.self, from: data)
+        return ownerInfo
+    }
+
+    func parseXml(_ xmlString: String) -> OwnerInfo {
+        let data = xmlString.data(using: .utf8)!
+        let ownerInfo: OwnerInfo = try! XMLDecoder().decode(OwnerInfo.self, from: data)
+        return ownerInfo
+    }
 }
--- Tests/parserTests/parserTests.swift
+++ Tests/parserTests/parserTests.swift
@@ -1,5 +1,5 @@
-import XCTest
 @testable import Parser
+import XCTest
 
 final class ParserTests: XCTestCase {
     func testExample() {
@@ -7,6 +7,34 @@ final class ParserTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(Parser().text, "Hello, World!")
+    }
+
+    func testParse() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+        var input = """
+        {"sigungu_cd":41410}
+        """
+        var want = OwnerInfo(sigunguCd: 41410)
+
+        XCTAssertEqual(Parser().parse(input), want)
+    }
+
+    func testParseXml() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+        var input = """
+        <item>
+            <sigungu_cd>41410</sigungu_cd>
+        </item>
+        """
+        var want = OwnerInfo(sigunguCd: 41410)
+
+        XCTAssertEqual(Parser().parseXml(input), want)
     }
 
     static var allTests = [
